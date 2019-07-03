d95d49e5e993d316c4474bbb5550898aa7820f74
Ole Hauer <ole@Oles-MacBook-Pro.local>
2019-07-03 17:42:20 +0200

    colors

--- src/Components/Card/CardStats.js
+++ src/Components/Card/CardStats.js
@@ -4,9 +4,9 @@ import './CardStats.css';
 const CardStats = props => {
   return (
 <div className="stats-container">
-  <div className="box a">{props.currentData.currentMP}/{props.data.maxMP}</div>
-  <div className="box b">{props.currentData.currentHP}/{props.data.maxHP}</div>
-  <div className="box c">{props.currentData.currentWP}/{props.data.maxWP}</div>
+  <div className="MP">{props.currentData.currentMP}/{props.data.maxMP}</div>
+  <div className="HP">{props.currentData.currentHP}/{props.data.maxHP}</div>
+  <div className="WP">{props.currentData.currentWP}/{props.data.maxWP}</div>
   <div className="box d">
     <button onClick={props.atk}>Attack</button>
   </div>
